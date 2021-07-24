b4e679f566f6b318830618751cf2544d3b3b2978
NicoMiguez <migueznico@gmail.com>
2021-07-24 12:28:31 -0300

    React events on TiraPeliculas.js

--- src/components/TiraPeliculas.js
+++ src/components/TiraPeliculas.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import Pelicula from './Pelicula';
 
 class TiraPeliculas extends Component {
+  
+  lanzarAlerta() {
+    alert('Me clickeaste!!');
+  }
+
+  cambiarAmarillo() {
+    document.querySelector('body').style.backgroundColor = 'Yellow'
+  }
+
+  cambiarRosa() {
+    document.querySelector('body').style.backgroundColor = 'Pink'
+  }
+  
+  
+  
   render() {
     return (
-      <div>
+      <div onClick = {this.lanzarAlerta} onMouseOver = {this.cambiarAmarillo} onMouseOut = {this.cambiarRosa}>
         <Pelicula generos={['Fantasia', 'Ciencia Ficcion']} titulo='Harry Potter' />
         <Pelicula rating={9.0} titulo='Inception' />
         <Pelicula generos={['Animada', 'Familiar']} rating={7.5} titulo='Toy Story' />
